c2c70e6444f85455711d6fa8348c48393cf97a4c
Agustín Stringa <stringaagustin1@gmail.com>
2021-12-14 18:29:53 -0300

    implementando back a la extension

--- app.js
+++ app.js
@@ -2,16 +2,240 @@
 /**todo lo demás (el addeventlistener del boton)
  * se ejecuta en la "pagina" de la extension, popup --> index.html
  */
-alert('codigo inyectado')
 
 
-// (function () {
-//     chrome.runtime.onConnect.addListener(function (port) {
-//         port.onMessage.addListener(async (message) => {
-//             const { action } = message;
-//             if (action == 'scrapingProfile') {
-//                 await scrapingProfile()
-//             }
-//         })
-//     })
-// })()
+
+
+const scrapingProfile = async () => {
+
+    //console.log('tab', tab);
+    //console.log('tab-id', id_tab);
+    //console.log('chrome', chrome);
+
+    //funcion para trabajar con titulos de yt
+    const resolveYT = () => {
+        console.log('===========IMPRIMIENDO SCRAPING DE YOUTUBE=======');
+        const videos = document.querySelectorAll('#video-title');
+        const arreglo = [];
+        for (let video of videos) {
+            arreglo.push(video.textContent);
+        }
+
+        Object.values(arreglo).forEach(titulo => {
+            console.log(titulo);
+        })
+    }
+
+    const sleep = function (seconds) {
+        return new Promise(function (resolve) {
+            setTimeout(function () {
+                resolve();
+            }, seconds * 1000);
+        }
+        );
+    }
+
+    const selectorProfile = {
+        'fullName': '.text-heading-xlarge',
+        'botonContacto': ".pb2 > span.pv-text-details__separator > a.ember-view",
+        'linkEmail': ".ci-email > div.pv-contact-info__ci-container > a.pv-contact-info__contact-link",
+        'birthday': '.ci-birthday > .pv-contact-info__ci-container > .pv-contact-info__contact-item',
+        'twitter_link': '.ci-twitter > ul > li.pv-contact-info__ci-container > a.pv-contact-info__contact-link',
+        'experienceItems': '#experience-section > ul > li',
+        'educationsItems': '#education-section > ul > li',
+        'country': 'div.pb2.pv-text-details__left-panel > .text-body-small',
+        'urlLinkedin': '.ci-vanity-url > .pv-contact-info__ci-container > a.pv-contact-info__contact-link'
+    }
+
+    let profile = {
+        experiences: [
+
+        ],
+        educations: []
+    };
+
+    const getInfo = async () => {
+        const { fullName, botonContacto, linkEmail, birthday, experienceItems, educationsItems, twitter_link, country, urlLinkedin } = selectorProfile;
+
+        profile.fullName = document.querySelector(fullName).textContent;
+        profile.location = (document.querySelector(country).textContent).replace("\\n", "").trim();
+
+        const items_experiencie = document.querySelectorAll(experienceItems);
+        const items_education = document.querySelectorAll(educationsItems);
+
+        let num_exp = 0;
+        let num_edu = 0;
+
+        //sacando info del boton de contacto
+        if (document.querySelector(botonContacto)) {
+            document.querySelector(botonContacto).click();
+
+            await sleep(2);
+
+            (document.querySelector(birthday)) ? profile.birthday = document.querySelector(birthday).innerText : null;
+            (document.querySelector(linkEmail)) ? profile.email = document.querySelector(linkEmail).href : null;
+            (document.querySelector(twitter_link)) ? profile.twitter_link = document.querySelector(twitter_link).href : null;
+            (document.querySelector(urlLinkedin)) ? profile.urlLinkedin = document.querySelector(urlLinkedin).href : null
+        }
+
+        await autoscrollToElement("body")
+        await sleep(4);
+        for (let item of items_experiencie) {
+
+            await sleep(1);
+            if (item) {
+                //alert('esta el item');
+            } else {
+                alert('no esta no se k pasa');
+            }
+
+            profile.experiences.push({})
+
+            item.getElementsByTagName('h3').length > 0 ? profile.experiences[num_exp].jobPosition = item.getElementsByTagName('h3')[0].innerText : null
+            item.getElementsByClassName('pv-entity__secondary-title').length > 0 ? profile.experiences[num_exp].companyName = item.getElementsByClassName('pv-entity__secondary-title')[0].innerText : null
+            item.getElementsByClassName("pv-entity__bullet-item-v2").length > 0 ? profile.experiences[num_exp].duration = item.getElementsByClassName("pv-entity__bullet-item-v2")[0].innerText : null
+            item.getElementsByClassName("pv-entity__description").length > 0 ? profile.experiences[num_exp].description = item.getElementsByClassName("pv-entity__description")[0].textContent : null
+
+            num_exp++;
+            // console.log('profile a envitar', profile);
+        }
+
+        for (let edu_item of items_education) {
+            await sleep(1);
+            if (edu_item) {
+
+            } else {
+                alert('no education');
+            }
+
+            profile.educations.push({});
+
+            edu_item.getElementsByClassName("pv-entity__school-name") ? profile.educations[num_edu].institutionName = edu_item.getElementsByClassName("pv-entity__school-name")[0].innerText : null;
+            if (edu_item.getElementsByClassName("pv-entity__comma-item").length > 0) {
+                edu_item.getElementsByClassName("pv-entity__comma-item").length > 1 ? profile.educations[num_edu].carreer = `${edu_item.getElementsByClassName("pv-entity__comma-item")[0].innerText} ${edu_item.getElementsByClassName("pv-entity__comma-item")[1].innerText}` : profile.educations[num_edu].carreer = `${edu_item.getElementsByClassName("pv-entity__comma-item")[0].innerText}`
+
+            }
+
+            num_edu++;
+        }
+        // console.log('profile a envitar', profile);
+
+
+
+
+        return profile;
+
+    }
+
+
+    const autoscrollToElement = async function (cssSelector) {
+
+        var exists = document.querySelector(cssSelector);
+
+        while (exists) {
+            //
+            let maxScrollTop = document.body.clientHeight - window.innerHeight;
+            let elementScrollTop = document.querySelector(cssSelector).offsetHeight
+            let currentScrollTop = window.scrollY
+
+
+            if (maxScrollTop == currentScrollTop || elementScrollTop <= currentScrollTop)
+                break;
+
+            await sleep(0.05)
+
+            let newScrollTop = Math.min(currentScrollTop + 20, maxScrollTop);
+            window.scrollTo(0, newScrollTop)
+
+
+        }
+
+        console.log('finish autoscroll to element %s', cssSelector);
+
+    };
+
+
+    //habria que crear un pop-up para que se "pinten" los datos scrapeados
+    /**
+     * solucion temporal a experiencies == 0, mandar a ejecutar de nuevo
+     */
+    return await getInfo();
+
+    // console.log("===========OBJETO CHROME=========", chrome);
+
+
+};
+
+
+const sendData = async (url, document) => {
+    try {
+        const fetchOptions = {
+            method: "POST",
+            body: document,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            mode: "cors",
+        };
+        const response = await fetch(url, fetchOptions);
+
+        const { data = {}, success } = await response.json();
+        console.log(data);
+        console.log("si llego hasta aca debe ir bastante bien");
+        if (success) return data;
+    } catch (error) {
+        console.log(
+            "🚀 ~ file: index.html ~ line 22 ~ sendData ~ error",
+            error
+        );
+        throw error;
+    }
+};
+
+async function processData(finalProfile) {
+    try {
+
+
+        const Data = [];
+        Data.push(finalProfile);
+
+        const stringData = JSON.stringify(Data);
+
+        const urlHost =
+            "http://localhost:8080/api/v1/profiles/addMultipleProfiles";
+
+        const responseMessage = await sendData(urlHost, stringData);
+        return responseMessage;
+    } catch (error) {
+        console.log(
+            "🚀 ~ file: index.html ~ line 102 ~ processData ~ error",
+            error
+        );
+        throw error;
+    }
+}
+
+
+
+(function () {
+    try {
+        chrome.runtime.onConnect.addListener(function (port) {
+            port.onMessage.addListener(async (message) => {
+                const { action } = message;
+                if (action == 'scrapingProfile') {
+                    const finalProfile = await scrapingProfile()
+                    console.log(finalProfile);
+                    //enviar perfil al backend
+                    const sending = await processData(finalProfile);
+                    console.log(sending);
+
+
+
+                }
+            })
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+})()
--- index.js
+++ index.js
@@ -1,204 +1,46 @@
 
-try {
-    // import scrapingProfile from "./app";
+// import scrapingProfile from "./app";
 
-    const btnScrap = document.getElementById('scrap-profile');
+const btnScrap = document.getElementById('scrap-profile');
 
-    btnScrap.addEventListener('click', async () => {
+btnScrap.addEventListener('click', async () => {
 
-        // retorna una promesa => console.log(chrome.tabs.query({ active: true, currentWindow: true }));
-        //"pide" por la tab que este activa y sea la que se visualiza. (o sea la que llama a la extension)
-        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    // retorna una promesa => console.log(chrome.tabs.query({ active: true, currentWindow: true }));
+    //"pide" por la tab que este activa y sea la que se visualiza. (o sea la que llama a la extension)
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
+    const port = chrome.tabs.connect(tab.id);
+    port.postMessage({ action: 'scrapingProfile' })
 
-        const port = chrome.tabs.connect(tab.id);
-        port.postMessage({ action: 'scrapingProfile' })
 
 
 
 
-        //tabletas me devuelve un array de todas las pestañas en la ventana
-        const tabletas = await chrome.tabs.query({});
-        console.log(tabletas);
-        //TAB HACE REFERENCIA A LA VENTANA DESDE LA QUE SE LLAMA LA EXTENSION
-        //NO LA DE LA EXTENSION
-        //SON DOS DISTINTAS
-        //tab se genera luego del await y la conexion
-        //por eso no es accesible en este contexto
-        if (tab !== null) {
-            //para pasar parametros en chrome.scripting se usa el indice args:[]
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                function: scrapingProfile,
-                //en este caso, le estoy pasando a la ventana "principal" datos de tab
-                //que vendria a ser ella misma
-                //por eso los datos de tab son de la ventana desde la que se llamo la extension
-                // args: [tab, tab.id]
-            });
-        }
+    //tabletas me devuelve un array de todas las pestañas en la ventana
+    const tabletas = await chrome.tabs.query({});
+    console.log(tabletas);
+    //TAB HACE REFERENCIA A LA VENTANA DESDE LA QUE SE LLAMA LA EXTENSION
+    //NO LA DE LA EXTENSION
+    //SON DOS DISTINTAS
+    //tab se genera luego del await y la conexion
+    //por eso no es accesible en este contexto
 
+    // if (tab !== null) {
+    //     //para pasar parametros en chrome.scripting se usa el indice args:[]
+    //     chrome.scripting.executeScript({
+    //         target: { tabId: tab.id },
+    //         function: scrapingProfile,
+    //         //en este caso, le estoy pasando a la ventana "principal" datos de tab
+    //         //que vendria a ser ella misma
+    //         //por eso los datos de tab son de la ventana desde la que se llamo la extension
+    //         // args: [tab, tab.id]
+    //     });
+    // }
 
-    });
 
+});
 
-    const scrapingProfile = () => {
-        //console.log('tab', tab);
-        //console.log('tab-id', id_tab);
-        //console.log('chrome', chrome);
 
 
-        //funcion para trabajar con titulos de yt
-        const resolveYT = () => {
-            console.log('===========IMPRIMIENDO SCRAPING DE YOUTUBE=======');
-            const videos = document.querySelectorAll('#video-title');
-            const arreglo = [];
-            for (let video of videos) {
-                arreglo.push(video.textContent);
-            }
-
-            Object.values(arreglo).forEach(titulo => {
-                console.log(titulo);
-            })
-        }
-
-        const sleep = function (seconds) {
-            return new Promise(function (resolve) {
-                setTimeout(function () {
-                    resolve();
-                }, seconds * 1000);
-            }
-            );
-        }
-
-        const selectorProfile = {
-            'name': '.text-heading-xlarge',
-            'botonContacto': ".pb2 > span.pv-text-details__separator > a.ember-view",
-            'linkEmail': ".ci-email > div.pv-contact-info__ci-container > a.pv-contact-info__contact-link",
-            'birthday': '.ci-birthday > .pv-contact-info__ci-container > .pv-contact-info__contact-item',
-            'experienceItems': '#experience-section > ul > li'
-        }
-
-        let profile = {
-            experiences: [
-
-            ]
-        };
-
-
-
-        const getInfo = async () => {
-            const { name, botonContacto, linkEmail, birthday, experienceItems } = selectorProfile;
-
-            profile.name = document.querySelector(name).textContent;
-
-            const items_experiencia = document.querySelectorAll(experienceItems);
-
-            let num_exp = 0;
-
-            //sacando info del boton de contacto
-            if (document.querySelector(botonContacto)) {
-                document.querySelector(botonContacto).click();
-
-                //await sleep(1.5);
-
-                (document.querySelector(birthday)) ? profile.birthday = document.querySelector(birthday).innerText : null;
-                (document.querySelector(linkEmail)) ? profile.email = document.querySelector(linkEmail).innerText : null;
-
-            }
-
-            await autoscrollToElement("body").then(async () => {
-
-                await sleep(4);
-
-                for (let index = 0; index < 10; index++) {
-                    console.log(index);
-
-                }
-
-                for (let item of items_experiencia) {
-
-                    if (item) {
-                        //alert('esta el item');
-                    } else {
-                        alert('no esta no se k pasa');
-                    }
-
-                    profile.experiences.push({})
-
-                    item.getElementsByTagName('h3').length > 0 ? profile.experiences[num_exp].titulo = item.getElementsByTagName('h3')[0].innerText : null
-                    item.getElementsByClassName('pv-entity__secondary-title').length > 0 ? profile.experiences[num_exp].empresa = item.getElementsByClassName('pv-entity__secondary-title')[0].innerText : null
-                    item.getElementsByClassName("pv-entity__bullet-item-v2").length > 0 ? profile.experiences[num_exp].duracion = item.getElementsByClassName("pv-entity__bullet-item-v2")[0].innerText : null
-                    item.getElementsByClassName("pv-entity__description").length > 0 ? profile.experiences[num_exp].descripcion = item.getElementsByClassName("pv-entity__description")[0].textContent : null
-
-                    num_exp++;
-                    // console.log('profile a envitar', profile);
-                }
-                // console.log('profile a envitar', profile);
-            })
-
-
-
-            return new Promise((res, rej) => {
-
-                /**
-                 * espera a hacer click para obtener elemento con el mail
-                 * esto debido a que no se presenta en el DOM de lo contrario
-                 */
-                res(profile);
-                rej('ha habido un error')
-                // setTimeout(() => {
-                //     res(profile);
-                //     rej('ha habido un error')
-                // }, 2000)
-
-            })
-
-        }
-
-
-        const autoscrollToElement = async function (cssSelector) {
-
-            var exists = document.querySelector(cssSelector);
-
-            while (exists) {
-                //
-                let maxScrollTop = document.body.clientHeight - window.innerHeight;
-                let elementScrollTop = document.querySelector(cssSelector).offsetHeight
-                let currentScrollTop = window.scrollY
-
-
-                if (maxScrollTop == currentScrollTop || elementScrollTop <= currentScrollTop)
-                    break;
-
-                await sleep(0.01)
-
-                let newScrollTop = Math.min(currentScrollTop + 20, maxScrollTop);
-                window.scrollTo(0, newScrollTop)
-
-
-            }
-
-            console.log('finish autoscroll to element %s', cssSelector);
-
-            return new Promise(function (resolve) {
-                resolve();
-            });
-        };
-
-
-        getInfo().then(data => { console.log(data) })
-
-
-
-        // console.log("===========OBJETO CHROME=========", chrome);
-
-
-
-
-    };
-
-} catch (error) {
-    alert(`el error dice ${error}`)
-}
 
 
